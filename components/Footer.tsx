13c1bad3bb842f0bec27e665d314d51f2b44fa02
a107285 <a107285@alunos.uminho.pt>
2024-03-13 22:42:18 +0000

    Mudanças pedidas

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,26 +2,24 @@ type PropsType = {
     viewCart:boolean
 }
 
-const footer = ({viewCart}:PropsType) => {
+const Footer = ({viewCart}:PropsType) => {
   
     const year : number = new Date().getFullYear()
     
     const content = (
         <footer className="min-h-screen flex flex-col justify-between ">
-            {viewCart ? (
-                <div className="absolute inset-x-0 bottom-0  p-6 flex items-center justify-center text-lg "> 
-                Shopping-cart &copy; {year}</div>
-            ) : (
-                <div className="absolute inset-x-0 bottom-0  p-6 flex flex-col items-center justify-center ">
+            {(!viewCart)&& (
+                <div className="absolute inset-x-0 bottom-12 p-6 flex flex-col items-center justify-center ">
                   <div className="flex">
                     <span className="text-2xl font-bold"> Total Items: &nbsp;</span> 
                      <span className="text-2xl "> XXX </span>
                     <span className="text-2xl font-bold ">&emsp;&emsp;Total Price:</span>  
                      <span className="text-2xl "> &nbsp;$XXX.X </span>
                  </div>
-                   <span className="text-lg ">Shopping-cart &copy; {year}   </span>
                 </div>
             )}
+               <div className="absolute inset-x-0 bottom-0  p-6 flex items-center justify-center text-lg "> 
+                Shopping-cart &copy; {year}</div>
         </footer>
     ); 
    
@@ -31,4 +29,4 @@ const footer = ({viewCart}:PropsType) => {
   )
 }
 
-export default footer 
+export default Footer 
